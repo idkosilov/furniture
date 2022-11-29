cacefb99d6365caa3d19d4f0d606354469979e09
Ivan Kosilov <idkosilov@gmail.com>
2022-11-30 02:50:26 +0700

    Refactored test for allocate

--- tests/test_allocation/unit/test_allocate.py
+++ tests/test_allocation/unit/test_allocate.py
@@ -10,9 +10,10 @@ later = today + datetime.timedelta(days=3)
 def test_prefers_current_stock_batches_to_shipments():
     in_stock_batch = model.Batch("in-stock-batch", "RETRO-CLOCK", 100, None)
     shipment_batch = model.Batch("shipment-batch", "RETRO-CLOCK", 100, tomorrow)
+    product = model.Product("RETRO-CLOCK", [in_stock_batch, shipment_batch])
     line = model.OrderLine("oref", "RETRO-CLOCK", 10)
 
-    model.allocate(line, [in_stock_batch, shipment_batch])
+    product.allocate(line)
 
     assert in_stock_batch.available_quantity == 90
     assert shipment_batch.available_quantity == 100
@@ -22,9 +23,10 @@ def test_prefers_earlier_batches():
     earliest = model.Batch("speedy-batch", "MINIMALIST-SPOON", 100, today)
     medium = model.Batch("normal-batch", "MINIMALIST-SPOON", 100, tomorrow)
     latest = model.Batch("slow-batch", "MINIMALIST-SPOON", 100, later)
+    product = model.Product("MINIMALIST-SPOON", [earliest, medium, latest])
     line = model.OrderLine("order1", "MINIMALIST-SPOON", 10)
 
-    model.allocate(line, [medium, earliest, latest])
+    product.allocate(line)
 
     assert earliest.available_quantity == 90
     assert medium.available_quantity == 100
@@ -35,7 +37,8 @@ def test_returns_allocated_batch_ref():
     in_stock_batch = model.Batch("in-stock-batch-ref", "HIGHBROW-POSTER", 100, None)
     shipment_batch = model.Batch("shipment-batch-ref", "HIGHBROW-POSTER", 100, tomorrow)
     line = model.OrderLine("oref", "HIGHBROW-POSTER", 10)
+    product = model.Product("HIGHBROW-POSTER", [in_stock_batch, shipment_batch])
 
-    allocation = model.allocate(line, [in_stock_batch, shipment_batch])
+    allocation = product.allocate(line)
 
     assert allocation == in_stock_batch.ref
