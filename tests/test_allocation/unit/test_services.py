86f5a9745daa16b9e358599559187e27f622730b
Ivan Kosilov <idkosilov@gmail.com>
2022-11-30 02:32:20 +0700

    Added tests for service add_batch and replaced sessions and repositories in api services with a uow

--- tests/test_allocation/unit/test_services.py
+++ tests/test_allocation/unit/test_services.py
@@ -4,56 +4,55 @@ import pytest
 
 from allocation.adapters import repository
 from allocation.domain import model
-from allocation.service_layer import services
+from allocation.service_layer import services, unit_of_work
 
 
 class FakeRepository(repository.AbstractRepository):
-
-    def __init__(self, batches: Iterable[model.Batch]):
+    def __init__(self, batches):
         self._batches = set(batches)
 
-    async def get(self, reference: str) -> Optional[model.Batch]:
-        return next(b for b in self._batches if b.reference == reference)
-
-    async def add(self, batch: model.Batch) -> None:
+    async def add(self, batch):
         self._batches.add(batch)
 
-    async def list(self) -> List[model.Batch]:
+    async def get(self, reference):
+        return next(b for b in self._batches if b.reference == reference)
+
+    async def list(self):
         return list(self._batches)
 
 
-class FakeSession:
-    committed = False
+class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
+    def __init__(self):
+        self.batches = FakeRepository([])
+        self.committed = False
 
     async def commit(self):
         self.committed = True
 
-
-@pytest.mark.asyncio
-async def test_commits():
-    line = model.OrderLine('o1', 'OMINOUS-MIRROR', 10)
-    batch = model.Batch('b1', 'OMINOUS-MIRROR', 100, None)
-    repo = FakeRepository([batch])
-    session = FakeSession()
-    await services.allocate(line, repo, session)
-    assert session.committed is True
+    async def rollback(self):
+        pass
 
 
 @pytest.mark.asyncio
-async def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, None)
-    repo = FakeRepository([batch])
-
-    result = await services.allocate(line, repo, FakeSession())
-    assert result == "b1"
+async def test_allocate_returns_allocation():
+    uow = FakeUnitOfWork()
+    await services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
+    result = await services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
+    assert result == "batch1"
 
 
 @pytest.mark.asyncio
-async def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
-    batch = model.Batch("b1", "AREALSKU", 100, None)
-    repo = FakeRepository([batch])
+async def test_allocate_errors_for_invalid_sku():
+    uow = FakeUnitOfWork()
+    await services.add_batch("b1", "AREALSKU", 100, None, uow)
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        await services.allocate(line, repo, FakeSession())
+        await services.allocate("o1", "NONEXISTENTSKU", 10, uow)
+
+
+@pytest.mark.asyncio
+async def test_allocate_commits():
+    uow = FakeUnitOfWork()
+    await services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
+    await services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
+    assert uow.committed
