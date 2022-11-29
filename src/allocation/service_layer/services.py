3e90132d415ff096f4e2127b10d03f59bc769d0f
Ivan Kosilov <idkosilov@gmail.com>
2022-11-30 02:32:59 +0700

    Added service add batch, changed services api with a uow

--- src/allocation/service_layer/services.py
+++ src/allocation/service_layer/services.py
@@ -1,7 +1,8 @@
-from typing import Iterable
+from datetime import date
+from typing import Iterable, Optional
 
-from allocation.adapters import repository
 from allocation.domain import model
+from allocation.service_layer import unit_of_work
 
 
 class InvalidSku(Exception):
@@ -12,10 +13,21 @@ def is_valid_sku(sku: str, batches: Iterable[model.Batch]):
     return sku in {b.stock_keeping_unit for b in batches}
 
 
-async def allocate(line: model.OrderLine, repo: repository.AbstractRepository, session) -> str:
-    batches = await repo.list()
-    if not is_valid_sku(line.stock_keeping_unit, batches):
-        raise InvalidSku(f"Invalid sku {line.stock_keeping_unit}")
-    batch_ref = model.allocate(line, batches)
-    await session.commit()
+async def add_batch(ref: str, sku: str, qty: int, eta: Optional[date], uow: unit_of_work.AbstractUnitOfWork):
+    async with uow:
+        await uow.batches.add(model.Batch(ref, sku, qty, eta))
+        await uow.commit()
+
+
+async def allocate(order_ref: str, sku: str, qty: int, uow: unit_of_work.AbstractUnitOfWork) -> str:
+    line = model.OrderLine(order_ref, sku, qty)
+    async with uow:
+        batches = await uow.batches.list()
+        if not is_valid_sku(sku, batches):
+            raise InvalidSku(f"Invalid sku {sku}")
+        batch_ref = model.allocate(line, batches)
+        await uow.commit()
     return batch_ref
+
+
+
